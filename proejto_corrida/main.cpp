e35a9e17ef36250c2189eaf5e2fcfdb199d8a6fe
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-07-09 21:07:24 -0300

    exibicao de texto adicionado

--- proejto_corrida/main.cpp
+++ proejto_corrida/main.cpp
@@ -19,15 +19,16 @@
 #else
 #include <GL/glut.h>
 #endif
-
+#include <stdio.h>
 #include <stdlib.h>
-
+void DesenhaTexto(char *, float , float , float , float , float );
 float rot = 0.0;
 float rot2 = 0.0;
 float ang = 0.0;
 float ang2 = 0.0; //angulo em relacao ao eixo X
 float p[3] = {0.0, 0.0, 5.0}; //posicao da camera
-float dir[3] = {1.0, 0.0, 0.0}; //direcao da camera --- x = cos (ang2)    y = sin (ang2)
+float dir[3] = {1.0, 0.0, 0.0}; //direcao da camera ---
+char texto[30];
 void eixos(float T)
 {
 
@@ -116,8 +117,9 @@ static void display(void)
     ang2 += ang;
     ang = 0.0;
 
-    dir[0]= cos(ang2);
-    dir[1]= sin(ang2);
+
+    dir[0]= sin(ang2*M_PI/180); //x
+    dir[1]= cos(ang2*M_PI/180); //y
 
     //criando eixos
     glPushMatrix();
@@ -147,6 +149,10 @@ static void display(void)
         glScalef(comp,20.0,1.0);
         face();
     glPopMatrix();
+
+    sprintf(texto,"penis");
+
+    DesenhaTexto(texto,500,200,0,0,0);
 
     glutSwapBuffers();
 }
@@ -187,11 +193,11 @@ static void key(unsigned char key, int x, int y)
         break;
 
         case '1':
-        ang -= 0.5;
+        ang -= 0.8;
         break;
 
         case '2':
-        ang += 0.5;
+        ang += 0.8;
         break;
     }
 
@@ -224,7 +230,7 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("GLUT Shapes");
+    glutCreateWindow("Corrida");
 
     glutReshapeFunc(resize);
     glutDisplayFunc(display);
@@ -256,4 +262,40 @@ int main(int argc, char *argv[])
     glutMainLoop();
 
     return EXIT_SUCCESS;
+}
+
+void DesenhaTexto(char *texto, float x, float y, float r, float g, float b)
+{
+    glDisable(GL_LIGHTING);    // se estiver usando a iluminação
+    glMatrixMode( GL_PROJECTION );
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(0, 640, 0, 480, -1.0, 1.0); // usar o mesmo tamanho da janela
+
+    /*
+    // background do texto
+    // fazer isso antes de desenhar o texto
+    int w = glutBitmapLength(GLUT_BITMAP_HELVETICA_18,(unsigned char*)texto); // largura do texto em px
+    float borda = 2; // folga pata borda
+    float h = 18; // altura da fonte
+    float d = h/4; // descendente para letras como "g", "j", "p", "q" e "y"
+
+    glColor3f(1-r,1-g,1-b); // escolher a cor
+    glBegin(GL_QUADS);
+        glVertex3f(x-borda,   y+borda+h, -0.5);
+        glVertex3f(x-borda,   y-borda-d , -0.5);
+        glVertex3f(x+borda+w, y-borda-d, -0.5);
+        glVertex3f(x+borda+w, y+borda+h, -0.5);
+    glEnd();
+    */
+
+    glColor3f(r,g,b);
+    glRasterPos2f(x, y);       // define a posição do texto
+    while(*texto)               // Exibe caracter a caracter
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*texto++);
+    }
+
+    glPopMatrix();
+    glEnable(GL_LIGHTING);     // se estiver usando a iluminação
 }
